6a149493d339061877be082a961a49207da9d13e
25267 <Alisher>
2021-11-25 19:00:54 +0600

    kafka producer

--- src/main/java/com/example/movieaccount/controller/MovieAccountController.java
+++ src/main/java/com/example/movieaccount/controller/MovieAccountController.java
@@ -3,6 +3,7 @@ package com.example.movieaccount.controller;
 import com.example.movieaccount.model.Account;
 import com.example.movieaccount.service.MovieAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,18 @@ import java.util.Optional;
 @RequestMapping("/user/account")
 public class MovieAccountController {
 
+    private static final String TOPIC = "movie_account";
+
+    @Autowired
+    private KafkaTemplate<String, Optional<Account>> kafkaTemplate;
+
     @Autowired
     private MovieAccountService service;
 
     @GetMapping("/{userId}")
     public Optional<Account> getAccount(@PathVariable("userId") Long userId) {
+
+        kafkaTemplate.send(TOPIC, service.getAccount(userId));
         return service.getAccount(userId);
     }
 }
